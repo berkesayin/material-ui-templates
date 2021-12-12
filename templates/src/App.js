13c63a21d6bd52661cbfc9ed7dc7bcc2ec0c08ff
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-12 18:08:11 +0300

    Create and design Chekcout page.

--- templates/src/App.js
+++ templates/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/SignIn/SignIn'
 import SignUp from './components/SignUp/SignUp'
+import Checkout from './components/Checkout/Checkout'
 
 function App() {
   return (
     <div>
-       <SignIn />
+       <Checkout />
     </div>
   );
 }
