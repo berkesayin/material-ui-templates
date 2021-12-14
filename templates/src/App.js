abea47341505c83041b2aec10001e3db4fb79feb
Berke Sayın <sayinberkesayin@gmail.com>
2021-12-14 18:03:12 +0300

    Add album part.

--- templates/src/App.js
+++ templates/src/App.js
@@ -4,11 +4,12 @@ import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import Checkout from './components/Checkout/Checkout';
 import Pricing from './components/Pricing/Pricing';
+import Album from './components/Album/Album';
 
 function App() {
   return (
     <div>
-       <Pricing />
+       <Album />
     </div>
   );
 }
